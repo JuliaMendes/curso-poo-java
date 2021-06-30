9f8655d9390b51e2337b6a96b4a9c9c462ee5727
JuliaMendes <juliadepaivams@gmail.com>
2021-06-30 16:41:41 -0300

    feat: novos exercicios poo

---
+++ src/Aula013/Aula013.java
@@ -0,0 +1,12 @@
+package Aula013;
+
+public class Aula013 {
+	public static void main(String[] args) {
+		Cachorro c = new Cachorro();
+		
+		c.reagir("Vai apanhar");
+		c.reagir(false);
+		c.reagir(11,45);
+		
+	}
+}
